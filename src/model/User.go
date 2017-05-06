cf6699958a6f43f9ea83dfcee322ca815639bdec
Ravi Prasad <ravi.delcj@gmail.com>
2017-05-07 01:32:46 +0530

    User Authentication module complete

--- src/model/User.go
+++ src/model/User.go
@@ -6,3 +6,13 @@ type User struct {
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 }
+
+type UserLogin struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type UserProfile struct {
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
--- src/user/user.go
+++ src/user/user.go
@@ -100,9 +100,128 @@ func RegisterUser(res http.ResponseWriter, req *http.Request) {
 //know that the user is signed in
 func LoginUser(res http.ResponseWriter, req *http.Request) {
 
+	res.Header().Set("Content-Type", "application/json")
+
+	if req.Method != "POST" {
+		fmt.Println("Not a Post Request")
+		initJson(0, "Not a Post Request")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	var user model.UserLogin
+
+	body, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		fmt.Println("Error in Reading")
+		initJson(0, "Invalid Data")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		fmt.Println("Invalid Keys")
+		initJson(0, "Invalid Keys")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	exists := databaseHelper.CheckUsernameExists(user.Username)
+
+	if !exists {
+		fmt.Println("Username doesnot exists")
+		initJson(0, "Username doesnot exists")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	//check if already logged in
+	var token string
+	err, token = databaseHelper.GetToken(user.Username)
+
+	if err != nil {
+		initJson(0, "Internal Error")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	var tokenExist bool
+	err, tokenExist = databaseHelper.CheckTokenInDb(token)
+
+	if tokenExist || err != nil {
+		fmt.Println("User Already Logged In")
+		initJson(0, "User Already Logged In")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	err, token = databaseHelper.CheckPassword(user)
+
+	if err != nil {
+		fmt.Println("Wrong Password")
+		initJson(0, "Wrong Password")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	returnToken := make(map[string]interface{})
+
+	returnToken["status"] = 1
+	returnToken["message"] = "User logged In"
+	returnToken["token"] = token
+	json.NewEncoder(res).Encode(returnToken)
 }
 
-//Resturns profile of a user
+//Takes username and token
+//Resturns firstname lastname of a user
 func UserProfile(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
 
+	if req.Method != "POST" {
+		initJson(0, "Not a POST request")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+
+	var user model.UserProfile
+	if err != nil {
+		initJson(0, "Error in Reading")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		initJson(0, "Error in Parsing")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	var exist bool
+
+	err, exist = databaseHelper.CheckTokenInDb(user.Token)
+
+	if err != nil || exist == false {
+		fmt.Println("User not present")
+		initJson(0, "User not present")
+		json.NewEncoder(res).Encode(jsonMap)
+		return
+	}
+
+	var firstname, lastname string
+
+	firstname, lastname, err = databaseHelper.GetUserDetail(user.Username)
+
+	resultMap := make(map[string]interface{})
+
+	resultMap["status"] = 1
+	resultMap["firstname"] = firstname
+	resultMap["lastname"] = lastname
+	json.NewEncoder(res).Encode(resultMap)
 }
